b41a643fc71a38f94d18d39f929895d7e127cb0f
Daniel Leal <daniel.leal@auto1.com>
2019-01-24 16:50:33 +0100

    Refine logout logic

--- app/src/main/java/com/dleal/kataloginout/MainActivity.kt
+++ app/src/main/java/com/dleal/kataloginout/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.dleal.kataloginout
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,24 +21,41 @@ class MainActivity : AppCompatActivity() {
 
             if (username == USERNAME && password == PASSWORD) {
                 //Success
-                login_group.hide()
-                btn_logout.show()
+                login()
             } else {
                 //Error
-                login_group.show()
-                btn_logout.hide()
+                Toast.makeText(this, "Invalid credentials", Toast.LENGTH_SHORT).show()
             }
+
+            hideKeyboard(this)
         }
+
+        btn_logout.setOnClickListener { logout() }
     }
+
+    private fun login() {
+        login_group.hide()
+        btn_logout.show()
+    }
+
+    private fun logout() {
+        login_group.show()
+        btn_logout.hide()
+
+        edit_username.clear()
+        edit_password.clear()
+    }
+
+    private fun hideKeyboard(activity: Activity) {
+        val inputMethodManager = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        //Find the currently focused view, so we can grab the correct window token from it.
+        val view = activity.currentFocus ?: View(activity)
+
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
 }
 
 private const val USERNAME = "admin"
 private const val PASSWORD = "admin"
-
-fun View.show(){
-    this.visibility = View.VISIBLE
-}
-
-fun View.hide(){
-    this.visibility = View.GONE
-}
